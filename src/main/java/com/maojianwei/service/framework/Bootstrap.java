0740b915046249defbbe5017b0b37b9e33c4da5b
Jianwei Mao <maojianwei2012@126.com>
2019-11-03 18:14:24 +0800

    Framework Entry ok, and can be an example for you to do some extension based on this service framework

--- src/main/java/com/maojianwei/service/framework/Bootstrap.java
+++ src/main/java/com/maojianwei/service/framework/Bootstrap.java
@@ -1,28 +1,49 @@
 package com.maojianwei.service.framework;
 
+import com.maojianwei.service.framework.core.MaoModuleManager;
 import com.maojianwei.service.framework.core.MaoRunningCore;
+import com.maojianwei.service.framework.core.web.MaoWebSystem;
 
 /**
- * Hello world!
- *
+ * Mao service framework (lightweight).
+ * @author Jianwei Mao  2019.11.03
  */
-public class Bootstrap
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class Bootstrap {
+    public static void main(String[] args) {
+
+        System.out.println("Mao service framework!");
 
         MaoRunningCore core = MaoRunningCore.getInstance();
         core.startPool();
 
+        MaoModuleManager moduleManager = MaoModuleManager.getInstance();
+
+        MaoWebSystem webSystem = new MaoWebSystem();
+        int webKey = moduleManager.registerModule(webSystem);
+
+
         synchronized (core) {
             try {
-                core.wait();
+                core.wait(); // wait for system shutdown signal.
             } catch (InterruptedException e) {
-                e.printStackTrace();
-                System.out.println("Wait exited.");
+                System.out.println("InterruptedException when core.wait().");
             }
         }
+
+
+        boolean webBool = moduleManager.unregisterModule(webSystem, webKey);
+        webBool = moduleManager.unregisterModule(webSystem, webKey);
+
+        core.stopPool();
+
+        try {
+            while (core.waitPoolFinish(500)) {}
+            System.out.println("Running Pool finish.");
+        } catch (InterruptedException e) {
+            System.out.println("InterruptedException when core.waitPoolFinish().");
+        }
+
+        System.out.println("Mao service framework exited.");
     }
 }
 
