4bd2bb6a7e80a22284a660a903e6ea3b4b24a4a6
Jianwei Mao <maojianwei2012@126.com>
2019-11-03 18:11:04 +0800

    abstract Task ok

--- src/main/java/com/maojianwei/service/framework/lib/MaoAbstractRunningTask.java
+++ src/main/java/com/maojianwei/service/framework/lib/MaoAbstractRunningTask.java
@@ -12,18 +12,24 @@ public class MaoAbstractRunningTask implements Runnable{
     public void run() {
         System.out.println(String.format("Activating %s ...", module.name()));
         module.activate();
+        System.out.println(String.format("Activated %s ...", module.name()));
 
-        while(!module.isNeedShutdown()) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                break;
-            }
+//        while(!module.isNeedShutdown()) {
+//            try {
+//                Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//                break; // mean that Framework goes shutdown.
+//            }
+//        }
+
+        try {
+            module.waitShutdown();
+        } catch (InterruptedException e) {
+            System.out.println(String.format("InterruptedException when module.waitShutdown() for %s", module.name()));
         }
 
         System.out.println(String.format("Deactivating %s ...", module.name()));
         module.deactivate();
-
         System.out.println(String.format("Closed %s .", module.name()));
     }
 }
